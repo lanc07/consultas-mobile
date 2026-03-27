e8a66c1326bc834ccf55e363016bb6d8f1f2a569
Allan <cardoso.souza2109@gmail.com>
2026-03-27 08:24:20 -0300

    feat: integrar a modelagem TypeScript do sistema ao aplicativo mobile

---
+++ src/interfaces/consulta.ts
@@ -0,0 +1,12 @@
+import { Paciente } from './../types/paciente';
+import { Medico } from "./medico";
+import { StatusConsulta } from "../types/statusConsulta";
+export interface Consulta {
+  id: number;
+  medico: Medico;
+  paciente: Paciente;
+  data: Date;
+  valor: number;
+  status: StatusConsulta;
+  observacoes?: string;
+}
